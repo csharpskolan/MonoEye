3b2b644b66742281efc7e596ba7c993a14deb458
Jonas Nilsson <jonilss@gmail.com>
2022-06-13 11:17:44 +0200

    initial commit

---
+++ MonoEye.Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonoEye.Game
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
